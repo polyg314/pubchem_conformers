1c2f8aac7638eb018979e287933b8147fe28fb6d
Paul Gaudin <paulgaudin314@gmail.com>
2020-11-23 19:16:46 -0800

    more parsing updates - _id

--- parser.py
+++ parser.py
@@ -45,23 +45,24 @@ def load_annotations(data_folder):
     for file in os.scandir(CSV_FILES):
         try: 
             lastid = -1
-            final_entry = False
+            final_entry = {}
             for chunk in pd.read_csv(file.path, names = ['CID1','CID2','ST','CT'], usecols = ['CID1','CID2'], chunksize=chunksize, header = None, dtype=dtypes):
                 for index, row in chunk.iterrows():
                     # print(row)
                     id1 = int(row['CID1'])
                     id2 = int(row['CID2'])
-                    if(id1 == lastid):
-                        final_entry["similar_conformers"].append(id2)
-                    else:
-                        if(final_entry):
-                            yield(final_entry)
-                            # print(final_entry)
-                        lastid = id1
-                        final_entry = {
-                            "_id": id1,
-                            "similar_conformers": [id2]
-                        }
+                    if(len(id1) > 0):
+	                    if(id1 == lastid):
+	                        final_entry["similar_conformers"].append(id2)
+	                    else:
+	                        if("_id" in final_entry):
+	                            yield(final_entry)
+	                            # print(final_entry)
+	                        lastid = id1
+	                        final_entry = {
+	                            "_id": id1,
+	                            "similar_conformers": [id2]
+	                        }
             if(len(final_entry["similar_conformer"]) == 1):
                 yield(final_entry)
                 # print(final_entry)
