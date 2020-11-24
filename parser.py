ea8e5f936c58bb85520cc31aa52ad4971139dd20
Paul Gaudin <paulgaudin314@gmail.com>
2020-11-24 00:25:30 -0800

    again

--- parser.py
+++ parser.py
@@ -44,7 +44,7 @@ def load_annotations(data_folder):
     dtypes = {'CID1': int, 'CID2': int, 'ST': int, 'CT': int}
     for file in os.scandir(CSV_FILES):
         try: 
-            # lastid = -1
+            lastid = -1
             # current_item = {}
             for chunk in pd.read_csv(file.path, names = ['CID1','CID2','ST','CT'], usecols = ['CID1','CID2'], chunksize=chunksize, header = None, dtype=dtypes):
                 for index, row in chunk.iterrows():
