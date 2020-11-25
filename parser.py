3b610b74f57ca3e2710cccea13db911ec7c0f205
Paul Gaudin <paulgaudin314@gmail.com>
2020-11-24 16:21:10 -0800

    grab all files in data_folder

--- parser.py
+++ parser.py
@@ -1,12 +1,13 @@
 import os
 import pandas as pd
 from .csvsort import csvsort
+from os.path import isfile, join
 
 def load_annotations(data_folder):
     col_names = ["CID1","LID1","CID2","LID2","ST","CT","Rxx","Rxy","Rxz","Ryx","Ryy","Ryz","Rzx","Rzy","Rzz","Tx","Ty","Tz"]
-    file_names = os.path.join(data_folder,'00000001_00500000x00000001_00500000.csv.gz')
+    # file_names = os.path.join(data_folder,'00000001_00500000x00000001_00500000.csv.gz')
+    file_names = [f for f in listdir(data_folder) if isfile(join(data_folder, f))]
     CSV_FILES = os.path.join(data_folder,"CSV_FILES")
-    dump_files = []
     interval_value = 1000000
     csv_ints = list(range(0,100000000,interval_value))
     try: 
@@ -22,17 +23,18 @@ def load_annotations(data_folder):
     pair_dict = {}
     chunksize = 10 ** 6
     smerge_counter = 0; # use merge counter to append file after file is created
-    for chunk in pd.read_csv(file_names, names = col_names, usecols = ['CID1','CID2','ST','CT'], chunksize=chunksize, header = 0):
-        new_entry = chunk.drop_duplicates()
-        ## ST or CT above 95, AND ST < 101
-        new_entry = new_entry[((new_entry["ST"] > 95) | (new_entry["CT"] > 95)) & (new_entry["ST"] < 101)]
-        new_entry.drop(["ST","CT"], axis=1)
-        rev_entry = new_entry.reindex(columns=['CID2','CID1'])
-        for x in csv_ints: 
-            temp_df = new_entry[(new_entry["CID1"] >= x) & (new_entry["CID1"] < (x + interval_value))]
-            temp_df.to_csv(path_or_buf=CSV_FILES + "/" + str(x) + ".csv", index=False, mode='a',header=False)
-            temp_df = rev_entry[(rev_entry["CID2"] >= x) & (rev_entry["CID2"] < (x + interval_value))]
-            temp_df.to_csv(path_or_buf=CSV_FILES + "/" + str(x) + ".csv", index=False, mode='a',header=False)
+    for file_name in file_names: 
+	    for chunk in pd.read_csv(file_name, names = col_names, usecols = ['CID1','CID2','ST','CT'], chunksize=chunksize, header = 0):
+	        new_entry = chunk.drop_duplicates()
+	        ## ST or CT above 95, AND ST < 101
+	        new_entry = new_entry[((new_entry["ST"] > 95) | (new_entry["CT"] > 95)) & (new_entry["ST"] < 101)]
+	        new_entry.drop(["ST","CT"], axis=1)
+	        rev_entry = new_entry.reindex(columns=['CID2','CID1'])
+	        for x in csv_ints: 
+	            temp_df = new_entry[(new_entry["CID1"] >= x) & (new_entry["CID1"] < (x + interval_value))]
+	            temp_df.to_csv(path_or_buf=CSV_FILES + "/" + str(x) + ".csv", index=False, mode='a',header=False)
+	            temp_df = rev_entry[(rev_entry["CID2"] >= x) & (rev_entry["CID2"] < (x + interval_value))]
+	            temp_df.to_csv(path_or_buf=CSV_FILES + "/" + str(x) + ".csv", index=False, mode='a',header=False)
 
     for file in os.scandir(CSV_FILES):
         try: 
